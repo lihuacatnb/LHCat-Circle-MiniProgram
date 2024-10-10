3c637943db1f25df2b30fe7720a2ddd624062889
狸猫科技 <zhaogangbjm@gmail.com>
2024-10-10 20:33:20 +0800

    Add files via upload

---
+++ unpackage/dist/build/mp-weixin/pages/message/message.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["pages/message/message"],{3375:function(n,t,e){},"3eb1":function(n,t,e){"use strict";var u=e("3375"),a=e.n(u);a.a},b704:function(n,t,e){"use strict";e.r(t);var u=e("de76"),a=e("d092");for(var o in a)["default"].indexOf(o)<0&&function(n){e.d(t,n,(function(){return a[n]}))}(o);e("3eb1");var r=e("828b"),i=Object(r["a"])(a["default"],u["b"],u["c"],!1,null,"8595e80c",null,!1,u["a"],void 0);t["default"]=i.exports},d092:function(n,t,e){"use strict";e.r(t);var u=e("f492"),a=e.n(u);for(var o in u)["default"].indexOf(o)<0&&function(n){e.d(t,n,(function(){return u[n]}))}(o);t["default"]=a.a},de76:function(n,t,e){"use strict";e.d(t,"b",(function(){return a})),e.d(t,"c",(function(){return o})),e.d(t,"a",(function(){return u}));var u={tnNavBar:function(){return Promise.all([e.e("common/vendor"),e.e("tuniao-ui/components/tn-nav-bar/tn-nav-bar")]).then(e.bind(null,"6911"))},tnBadge:function(){return Promise.all([e.e("common/vendor"),e.e("tuniao-ui/components/tn-badge/tn-badge")]).then(e.bind(null,"21d4"))}},a=function(){var n=this.$createElement;this._self._c},o=[]},f492:function(n,t,e){"use strict";(function(n){Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var e={name:"Message",data:function(){return{}},onLoad:function(){},methods:{tn:function(t){n.navigateTo({url:t})}}};t.default=e}).call(this,e("df3c")["default"])}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'pages/message/message-create-component',
+    {
+        'pages/message/message-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('df3c')['createComponent'](__webpack_require__("b704"))
+        })
+    },
+    [['pages/message/message-create-component']]
+]);
